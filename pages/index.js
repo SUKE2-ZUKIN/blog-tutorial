2e9e42994a5fdf1ffb9c00b66b602113063d2657
SUKE2-ZUKIN <mono.316b@gmail.com>
2022-12-25 16:15:23 +0900

    #5 Add meta data

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Meta from 'components/meta'
 
 export default function Home() {
   return (
     <Container>
+      <Meta />
+
       <Hero 
         title={"React Blog"}
         subtitle={"Next.js / Reactについてアウトプットしていきます。"}
