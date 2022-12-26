61b69624436d0e1f19a325478523e0cb9d464845
SUKE2-ZUKIN <mono.316b@gmail.com>
2022-12-26 12:12:47 +0900

    #5 Add micro-cms

---
+++ pages/blog/react.js
@@ -0,0 +1,32 @@
+import Container from "components/container";
+import { getPostBySlug  } from "lib/api";
+
+export default function Schedule({
+  title,
+  publish,
+  content,
+  eyecatch,
+  categories,
+}) {
+  return (
+    <Container>
+      <h1>{title}</h1>
+    </Container>
+  )
+}
+
+export async function getStaticProps() {
+  const slug = "react"
+
+  const post = await getPostBySlug(slug)
+  console.log(post);
+  return {
+    props: {
+      title: post.title,
+      publish: post.publishDate,
+      content: post.content,
+      eyecatch: post.eyecatch,
+      categories: post.categories,
+    },
+  }
+}
