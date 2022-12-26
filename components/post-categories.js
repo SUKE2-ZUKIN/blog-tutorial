b2bbf5932c458e872b668056e089a67576351cf4
SUKE2-ZUKIN <mono.316b@gmail.com>
2022-12-27 07:09:28 +0900

    #7 Add post body

---
+++ components/post-categories.js
@@ -0,0 +1,22 @@
+import { faFolderOpen } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import styles from "styles/post-categories.module.css";
+
+export default function PostCategories({ categories }) {
+  return (
+    <div className={styles.flexContainer}>
+      <h3 className={styles.heading}>
+        <FontAwesomeIcon icon={faFolderOpen} />
+        <span  className="sr-only">Categories</span>
+      </h3>
+      <ul className={styles.list}>
+        {categories.map(({ slug }) => (
+          <li key={slug}>
+            <Link href={`/blog/category/${slug}`} ></Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- pages/blog/react.js
+++ pages/blog/react.js
@@ -1,5 +1,13 @@
 import Container from "components/container";
+import ConvertBody from "components/convert-body";
+import Meta from "components/meta";
+import PostBody from "components/post-body";
+import PostCategories from "components/post-categories";
+import PostHeader from "components/post-header";
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from "components/two-column";
 import { getPostBySlug  } from "lib/api";
+import { extractText } from "lib/extract-text";
+import Image from "next/legacy/image";
 
 export default function Schedule({
   title,
@@ -7,10 +15,48 @@ export default function Schedule({
   content,
   eyecatch,
   categories,
+  description,
 }) {
   return (
     <Container>
-      <h1>{title}</h1>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pageImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+      
+      <article>
+        <PostHeader
+          title={title}
+          subtitle="Blog Articles"
+          publish={publish}
+        />
+
+        <figure>
+          <Image
+            src={eyecatch.url}
+            alt="アイキャッチ画像"
+            layout="responsive"
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes="(min-witdh: 1152px) 1152px, 100vw"
+            priority
+          />
+        </figure>
+
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={content} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar>
+            <PostCategories categories={categories} />
+          </TwoColumnSidebar>
+        </TwoColumn>
+      </article>
     </Container>
   )
 }
@@ -19,7 +65,8 @@ export async function getStaticProps() {
   const slug = "react-basic"
 
   const post = await getPostBySlug(slug)
-  console.log(post);
+
+  const description = extractText(post.content)
   return {
     props: {
       title: post.title,
@@ -27,6 +74,7 @@ export async function getStaticProps() {
       content: post.content,
       eyecatch: post.eyecatch,
       categories: post.categories,
+      description: description,
     },
   }
 }
